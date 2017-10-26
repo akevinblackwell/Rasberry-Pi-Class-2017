39d6fcfaa3845e61e2a8a29310e44bdb184e4a8f
CB0916 <cblackwell0916@gmail.com>
2017-10-26 12:32:21 -0400

    Changes for Relay

--- blinky.py
+++ blinky.py
@@ -1,10 +1,7 @@
 import time
 import RPi.GPIO as GPIO
 
-BlueLED = 22
-RedLED = 15
-GreenLED = 13
-YellowLED = 11
+BlueLED = 3
 
 blinkrate = 0.0
 
@@ -13,17 +10,10 @@ pushbutton = 18
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(BlueLED, GPIO.OUT)
-GPIO.setup(RedLED, GPIO.OUT)
-GPIO.setup(GreenLED, GPIO.OUT)
-GPIO.setup(YellowLED, GPIO.OUT)
-GPIO.setup(pushbutton, GPIO.IN)
 
 # initialize all LEDs to off
 
 GPIO.output(BlueLED, GPIO.LOW)
-GPIO.output(RedLED, GPIO.LOW)
-GPIO.output(GreenLED, GPIO.LOW)
-GPIO.output(YellowLED, GPIO.LOW)
 
 # ask for the blinkrate
 
@@ -32,25 +22,10 @@ blinkrate=input("Enter your blinkrate:  ")
 while True:
 #while GPIO.input(pushbutton) == True:
 	GPIO.output(BlueLED, GPIO.HIGH)
-	GPIO.output(RedLED, GPIO.LOW)
-	GPIO.output(GreenLED, GPIO.LOW)
-	GPIO.output(YellowLED, GPIO.LOW)
 	time.sleep(blinkrate)
 	GPIO.output(BlueLED, GPIO.LOW)
-	GPIO.output(RedLED, GPIO.HIGH)
-	GPIO.output(GreenLED, GPIO.LOW)
-	GPIO.output(YellowLED, GPIO.LOW)
 	time.sleep(blinkrate)
-	GPIO.output(BlueLED, GPIO.LOW)
-	GPIO.output(RedLED, GPIO.LOW)
-	GPIO.output(GreenLED, GPIO.HIGH)
-	GPIO.output(YellowLED, GPIO.LOW)
-	time.sleep(blinkrate)
-	GPIO.output(BlueLED, GPIO.LOW)
-	GPIO.output(RedLED, GPIO.LOW)
-	GPIO.output(GreenLED, GPIO.LOW)
-	GPIO.output(YellowLED, GPIO.HIGH)
-	time.sleep(blinkrate)
+	
 #	print GPIO.input(pushbutton)
 
 print "Button Pushed"
@@ -58,6 +33,3 @@ print "Button Pushed"
 # initialize all LEDs to off
 
 GPIO.output(BlueLED, GPIO.LOW)
-GPIO.output(RedLED, GPIO.LOW)
-GPIO.output(GreenLED, GPIO.LOW)
-GPIO.output(YellowLED, GPIO.LOW)
